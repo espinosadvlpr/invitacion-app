1c66887ebdbbfbf595ad64e0606308cc8ad01d0d
David Espinosa <espinosadavid47@gmail.com>
2022-03-05 14:20:43 -0500

    (v1.0): design and construction

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
+import image from './memeface.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> React app </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>INVITACION FACHERA</h1>
+      <p>Buenas tardes don Ivan, </p>
+      <p>La presente es para pedir el permiso para que la señorita Paula Valeria Sanchez Nieto asista al cine el dia 05 de Marzo del 2022</p>
+      <p>Lugar: Royal films - VIVA Tunja</p>
+      <p>Hora: 9 p.m.</p>
+      <p>Hora de llegada: 12:30 p.m.</p>
+      <p>Muchas gracias, quedo atento a su respuesta.</p>
+      <img src={image}/>
     </div>
   );
 }
